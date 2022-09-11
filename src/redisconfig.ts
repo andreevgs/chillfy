a5d36d7f6d7ab7c2c1dc14730d958a592ee39733
AndreevG <andreev.gs12@yandex.by>
2022-09-11 23:08:55 +0300

    edit redisconfig.ts

--- src/redisconfig.ts
+++ src/redisconfig.ts
@@ -4,6 +4,9 @@ dotenv.config();
 const config = {
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT),
+    tls: {
+        rejectUnauthorized: true
+    }
 };
 
 export default config;
