58d75fc64108b02f9df8a132fe2a7c3449ce75cc
AndreevG <andreev.gs12@yandex.by>
2022-09-12 00:59:13 +0300

    edit redisconfig.ts

--- src/redisconfig.ts
+++ src/redisconfig.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const config = {
     host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT),
+    // port: parseInt(process.env.REDIS_PORT),
 };
 
 export default config;
