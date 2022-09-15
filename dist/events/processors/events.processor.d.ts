4436b6375923030b48895edee9b8c5e67cc27af3
AndreevG <andreev.gs12@yandex.by>
2022-09-15 19:37:55 +0300

    add reminders

--- dist/events/processors/events.processor.d.ts
+++ dist/events/processors/events.processor.d.ts
@@ -1,7 +1,10 @@
 import { Job } from 'bull';
 import { EventsService } from "../events.service";
+import { EmailingService } from "../../emailing/emailing.service";
 export declare class EventsProcessor {
     private readonly eventsService;
-    constructor(eventsService: EventsService);
+    private readonly emailingService;
+    constructor(eventsService: EventsService, emailingService: EmailingService);
     deleteEvent(job: Job): Promise<void>;
+    remindAboutEvent(job: Job): Promise<void>;
 }
--- dist/events/processors/events.processor.js
+++ dist/events/processors/events.processor.js
@@ -12,13 +12,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.EventsProcessor = void 0;
 const bull_1 = require("@nestjs/bull");
 const events_service_1 = require("../events.service");
+const emailing_service_1 = require("../../emailing/emailing.service");
 let EventsProcessor = class EventsProcessor {
-    constructor(eventsService) {
+    constructor(eventsService, emailingService) {
         this.eventsService = eventsService;
+        this.emailingService = emailingService;
     }
     async deleteEvent(job) {
         console.log('deletion');
         await this.eventsService.deleteEvent(job.data.req, job.data.eventId);
+    }
+    async remindAboutEvent(job) {
+        console.log('remind');
+        this.emailingService.sendMail({
+            to: job.data.invitation.user.email,
+            subject: `Reminding about ${job.data.invitation.event.name}`,
+            text: `We remind you that the event has begun!`
+        });
     }
 };
 __decorate([
@@ -27,9 +37,16 @@ __decorate([
     __metadata("design:paramtypes", [Object]),
     __metadata("design:returntype", Promise)
 ], EventsProcessor.prototype, "deleteEvent", null);
+__decorate([
+    (0, bull_1.Process)('remind'),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], EventsProcessor.prototype, "remindAboutEvent", null);
 EventsProcessor = __decorate([
     (0, bull_1.Processor)('events'),
-    __metadata("design:paramtypes", [events_service_1.EventsService])
+    __metadata("design:paramtypes", [events_service_1.EventsService,
+        emailing_service_1.EmailingService])
 ], EventsProcessor);
 exports.EventsProcessor = EventsProcessor;
 //# sourceMappingURL=events.processor.js.map
